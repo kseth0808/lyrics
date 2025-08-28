dab4d59e11c62e02c97430fe0c01ec780a707948
Kushagra <kseth0808@gmail.com>
2025-08-28 18:05:38 +0530

    fixed

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get("/", (req, res) => {
 app.use('/data', apps);
 
 
-app.listen(3001, () => {
-    console.log(`✅ Server started on http://localhost`);
-}); 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
